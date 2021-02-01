fe8b6d2fa0f33d5f49cfc486713079584e5b77fc
Carlos León <mail@carlosleon.info>
2021-02-01 07:15:01 +0100

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-IMG=mongrelion/blog
-TAG=$(git log -1 --pretty=%H)
-ssh carlosleon.info <<EOF
-docker pull ${IMG}:${TAG}
-docker rm -f blog
-docker run -d --name blog -p 80:80 -p 443:443 ${IMG}:${TAG}
-docker ps
-EOF
+scp -r public/* carlosleon.info:/var/www/blog
